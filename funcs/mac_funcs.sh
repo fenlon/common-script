7ca44e88d0f0c32e73b64970915a20a1e3e83c8a
熊凤龙 <fenglongxiong@anjuke.com>
2017-01-19 15:33:14 +0800

    first commit

---
+++ funcs/mac_funcs.sh
@@ -0,0 +1,45 @@
+TJ_HOST_HOME=/Users/fenlon/.tj_host
+
+function pwdx {
+  lsof -a -p $1 -d cwd -n | tail -1 | awk '{print $NF}'
+}
+
+function tj {
+  cat $TJ_HOST_HOME 
+}
+
+function to {
+  if [ $# != 1 ];then
+      echo "Usage: to [hostId]"
+      return
+  fi
+  
+  hostId=$1
+  
+  OLD_IFS=$IFS
+  IFS='
+  '
+  not_found="ip not found"
+  remoteIp=$not_found
+  for line in  $(cat $TJ_HOST_HOME) 
+  do
+      if [ ${#line} != 0 -a ${line:0:1} != "[" -a ${line: -1} != "]" ];then
+         
+         host_id="$(echo "$line"|awk -F '\t' '{print $1}')"
+         ip="$(echo "$line"|awk -F '\t' '{print $2}')"
+         if [ "$host_id" = "$hostId" ];then
+            remoteIp=$ip
+            break
+         fi
+      fi
+  done  
+  
+  IFS=$OLD_IFS
+  
+  if [ $remoteIp = $not_found ];then
+     echo $not_found
+     return
+  fi
+  echo "final goto $remoteIp"
+  ssh work@$remoteIp
+}
